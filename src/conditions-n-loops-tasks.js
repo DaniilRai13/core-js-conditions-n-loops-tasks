fc395b366abdcef8070d0985972d03139cb3ce48
DaniilRai13 <testnet02@mail.ru>
2024-02-13 02:31:54 +0300

    feat: complite tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -280,8 +280,21 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  let firstSum = 0;
+  for (let i = 0; i < arr.length - 2; i += 1) {
+    let sum = 0;
+    firstSum += arr[i];
+    let j = i + 2;
+    do {
+      sum += arr[j];
+      j += 1;
+    } while (j < arr.length);
+    if (firstSum === sum) {
+      return i + 1;
+    }
+  }
+  return -1;
 }
 
 /**
